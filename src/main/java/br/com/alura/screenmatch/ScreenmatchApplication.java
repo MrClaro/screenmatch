a9d0d6f4f9480d34a451269628a4108639616785
MrClaro <adryan.contatoprofissional@gmail.com>
2025-06-19 00:31:50 -0300

    feat(main): realocado funcionalidades para classe principal

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,9 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
+import br.com.alura.screenmatch.client.Principal;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -16,12 +14,10 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
   @Override
   public void run(String... args) throws Exception {
-    var consumoApi = new ConsumoApi();
-    var json = consumoApi.obterDados("http://www.omdbapi.com/?t=gilmore+girls&apikey=[API-KEY]");
-    System.out.println(json);
-    ConverteDados converteDados = new ConverteDados();
-    DadosSerie dadosSerie = converteDados.obterDados(json, DadosSerie.class);
-    System.out.println(dadosSerie);
+
+    Principal principal = new Principal();
+    principal.exibeMenu();
+
   }
 
 }
